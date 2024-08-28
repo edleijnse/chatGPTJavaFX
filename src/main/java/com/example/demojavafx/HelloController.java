0009479ffa348488ab48761a4d9e0dbb66fe4557
edleijnse <ed@leijnse.info>
2024-08-28 21:57:04 +0200

    First Version of ChatGPTJavaFX with copy example

--- src/main/java/com/example/demojavafx/HelloController.java
+++ src/main/java/com/example/demojavafx/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.demojavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private TextField textQuestion;
+
+    @FXML
+    private TextArea textareaAnswer;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        // Copy the content of textQuestion to textareaAnswer
+        textareaAnswer.setText(textQuestion.getText());
+    }
+
+    @FXML
+    protected void onCleanButtonClick() {
+        // Clear the contents of textQuestion and textareaAnswer
+        textQuestion.setText("");
+        textareaAnswer.setText("");
     }
 }
